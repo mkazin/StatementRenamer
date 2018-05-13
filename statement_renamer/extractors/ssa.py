c9c63491bd096e3761b9ea5049574d6cd26617d8
Michael J. Kazin <mkazin@gmail.com>
2018-05-13 15:18:36 -0400

    Implemented extractor for yearly Social Security statements

---
+++ statement_renamer/extractors/ssa.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+from .extractor import DateExtractor, ExtractedData, ExtractorException
+
+
+class SocialSecurityExtractorException(ExtractorException):
+
+    def __init__(self, *args, **kwargs):
+        ExtractorException.__init__(self, *args, **kwargs)
+
+
+class SocialSecurityDateExtractor(DateExtractor):
+
+    EXCEPTION = SocialSecurityExtractorException
+    SEARCH_TEXT = 'Your estimated taxable earnings per year after '
+    FILE_FORMAT = '{} Yearly Statement.pdf'
+
+    @staticmethod
+    def match(text):
+        return (__class__.SEARCH_TEXT in text)
+
+    def extract(self, text):
+
+        start = 0
+        while True:
+            start = text.find(self.__class__.SEARCH_TEXT, start + 1)
+            extracted = text[start:start + len(self.__class__.SEARCH_TEXT) + 4]
+
+            if start < 0:
+                raise self.__class__.EXCEPTION(
+                    type(self).__name__ + ': Expected text not found')
+
+            try:
+                parts = extracted.split(' ')
+
+                start += len(self.__class__.SEARCH_TEXT)
+
+                start_date = datetime(int(parts[-1]), 1, 1)
+                end_date = datetime(int(parts[-1]), 12, 31)
+                return ExtractedData(start_date, end_date)
+
+            except ValueError:
+                print("ValueError at index: {} - [{}]".format(start, extracted))
+                pass
+
+    def rename(self, extracted_data):
+        return self.__class__.FILE_FORMAT.format(
+            extracted_data.get_end_date().year)
